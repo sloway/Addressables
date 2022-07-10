a6a001ecaa27a392e971e5d005af22344cf949c6
touchthesky80 <touchthesky80@gmail.com>
2022-07-10 16:26:20 +0900

    Instantiate workbench prefab on viewer project

--- AddressablesViewer/Assets/Scripts/BundleLoader.cs
+++ AddressablesViewer/Assets/Scripts/BundleLoader.cs
@@ -1,18 +1,32 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Threading.Tasks;
 using UnityEngine;
+using UnityEngine.AddressableAssets;
+using System.Linq;
 
 public class BundleLoader : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    private async void Start()
     {
-        
+        var catalogPath = "http://127.0.0.1:5500/StandaloneWindows64/catalog_2022.07.10.06.57.36.json";
+        var key = "Workbench";
+        await LoadBundlePrefab(catalogPath, key);
     }
 
-    // Update is called once per frame
-    void Update()
+    private async Task LoadBundlePrefab(string catalogPath, string key)
     {
-        
+        await Addressables.LoadContentCatalogAsync(catalogPath).Task;
+
+        var prefabs = await Addressables.LoadResourceLocationsAsync(key, typeof(GameObject)).Task;
+
+        var prefab = prefabs.First(item => item.InternalId.EndsWith(".prefab"));
+
+        Debug.Log(prefab);
+
+        await Addressables.InstantiateAsync(prefab).Task;
+
+        Debug.Log("Done");
     }
+
 }
